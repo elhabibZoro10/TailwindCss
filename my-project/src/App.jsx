7736c0f6f7797242b1e4ad17bc5515b6e45b59cc
ELhabib <elhabibbennani10@gmail.com>
2024-07-23 12:05:07 +0100

    Establishing the project infrastructure

---
+++ my-project/src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <h1 className="text-mainColor text-3xl font-bold underline">
+      Hello world!
+    </h1>
+  );
+};
+
+export default App;
