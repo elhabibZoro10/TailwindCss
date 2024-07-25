dd0f0cd274aa558d8379b16030fd340fadf8244b
ELhabib <elhabibbennani10@gmail.com>
2024-07-25 13:27:46 +0100

    add image on bottom Landing and create design of Features page

---
+++ my-project/src/components/Features.jsx
@@ -0,0 +1,46 @@
+import { useState } from "react";
+import FeatureBox from "./FeatureBox";
+
+const Features = () => {
+  const [items, setItems] = useState([
+    {
+      icon: "icon-access-anywhere.svg",
+      title: "Access your files, anywhere",
+      desc: "The ability to use a smartphone, tablet, or computer to access your account means your files follow you everywhere.",
+    },
+    {
+      icon: "icon-security.svg",
+      title: "Security you can trust",
+      desc: "2-factor authentication and user-controlled encryption are just a couple of the security features we allow to help secure your files.",
+    },
+    {
+      icon: "icon-collaboration.svg",
+      title: "Real-time collaboration",
+      desc: "Securely share files and folders with friends, family and colleagues for live collaboration. No email attachments required.",
+    },
+    {
+      icon: "icon-any-file.svg",
+      title: "Store any type of file",
+      desc: "Whether you're sharing holidays photos or work documents, Fylo has you covered allowing for all file types to be securely stored and shared",
+    },
+  ]);
+
+  return (
+    <section>
+      <div className="container">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-[100px] w-[865px] mx-auto max-w-full ">
+          {items.map((item) => (
+            <FeatureBox
+              key={item.title}
+              title={item.title}
+              icon={item.icon}
+              desc={item.desc}
+            />
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Features;
--- my-project/src/components/Landing.jsx
+++ my-project/src/components/Landing.jsx
@@ -27,6 +27,13 @@ const Landing = () => {
           </a>
         </div>
       </div>
+      <div className="w-full h-[200px]">
+        <img
+          src="/src//assets/images/bg-curvy-desktop.svg"
+          alt="img"
+          className="w-full h-full"
+        />
+      </div>
     </section>
   );
 };
